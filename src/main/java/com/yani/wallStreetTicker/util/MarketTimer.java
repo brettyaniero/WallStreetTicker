537b85073dcdfb0f2557cb2f8041f308dd28aae2
Brett Yaniero <brett.yaniero@ndus.edu>
2018-01-30 00:50:22 -0600

    Added timer class

---
+++ src/main/java/com/yani/wallStreetTicker/util/MarketTimer.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.TimerTask;
+import repositories.QuoteData;
+
+public class MarketTimer extends TimerTask {
+	QuoteData subject;
+
+	public MarketTimer(QuoteData subject) {
+		this.subject = subject;
+	}
+
+	public void run() {
+		subject.refreshData();
+	}
+}
